685dddb410a187aabe03268f9a40b87194fb4944
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-04-25 17:17:02 -0500

    add test route for sending texts

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const http = require('http');
 const express = require("express");
@@ -11,6 +13,20 @@ const app = express();
 app.get('/', (req, res) => {
   res.send("Test Route");
   res.end();
+})
+
+app.get('/sms', (req, res) => {
+
+  const client = require('twilio')(accountSid, authToken);
+  client.messages
+    .create({
+      body: 'hi from nyc pee',  
+      messagingServiceSid: 'MG1d6d31b1fb9fba83d482f516be369835',      
+      to: '+19728541675' 
+    })
+    .then(message => console.log(message.sid))
+    .done();
+
 })
 
 app.post('/sms', (req, res) => {
