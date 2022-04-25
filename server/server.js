2c6b4daabc64d9859bc8a6c4120215371dce896b
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-04-25 17:56:13 -0500

    reorg server.js and serve up front end

--- server/server.js
+++ server/server.js
@@ -1,42 +1,30 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
 const http = require('http');
+const bodyParser = require('body-parser');
 const express = require("express");
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const cors = require('cors');
 
 const app = express();
+const router = require('./router');
 
-app.get('/', (req, res) => {
-  res.send("Test Route");
-  res.end();
-})
+app.use(cors());
 
-app.get('/sms', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  const client = require('twilio')(accountSid, authToken);
-  client.messages
-    .create({
-      body: 'hi from nyc pee',  
-      messagingServiceSid: 'MG1d6d31b1fb9fba83d482f516be369835',      
-      to: '+19728541675' 
-    })
-    .then(message => console.log(message.sid))
-    .done();
+router(app);
 
-})
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-
-  twiml.message('Testing...');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
-})
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 
