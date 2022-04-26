eb73e4f72f4f86ebfe952f0a23a852008198d863
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-04-26 15:23:33 -0500

    add front end log in

--- server/client/src/components/auth/SignIn.js
+++ server/client/src/components/auth/SignIn.js
@@ -1,22 +1,47 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+const userSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required()
+});
+
 const SignIn = () => { 
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(userSchema)
+  });
+  
+  const handleFormSubmit = (data) => {
+    console.log(data);
+    // call action that will sign in user if it exists in database
+  };
+  
   return (
-    <form>
-    <div className='form-group'>
-      <label>Email</label>
-      <input
-        className='form-control'
-        name='email'></input>
-    </div>
-
-    <div className="form-group">
-      <label>Password</label>
-      <input 
-        className="form-control"
-        name='password' ></input>
-    </div>
-
-    <button className="btn btn-primary" type="submit">Submit</button>
-  </form>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <div className='form-group'>
+        <label>Email</label>
+        <input
+          className='form-control'
+          name='email'
+          {...register("email", {
+            required: "Required",
+          })}/>
+          {errors.email?.message}
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input 
+          className="form-control"
+          name='password' 
+          {...register("password", {
+            required: "Required",
+          })}/>
+          {errors.password?.message}
+      </div>
+      <button className="btn btn-primary" type="submit">Submit</button>
+    </form>
 )
 }
 
--- server/client/src/components/auth/SignUp.js
+++ server/client/src/components/auth/SignUp.js
@@ -1,5 +1,48 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+const userSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required()
+});
+
 const SignUp = () => { 
-  return <div>Sign Up</div>
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(userSchema)
+  });
+  
+  const handleFormSubmit = (data) => {
+    console.log(data);
+    // call action that will sign in user if it exists in database
+  };
+  
+  return (
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
+      <div className='form-group'>
+        <label>Email</label>
+        <input
+          className='form-control'
+          name='email'
+          {...register("email", {
+            required: "Required",
+          })}/>
+          {errors.email?.message}
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input 
+          className="form-control"
+          name='password' 
+          {...register("password", {
+            required: "Required",
+          })}/>
+          {errors.password?.message}
+      </div>
+      <button className="btn btn-primary" type="submit">Submit</button>
+    </form>
+)
 }
 
 export default SignUp;
