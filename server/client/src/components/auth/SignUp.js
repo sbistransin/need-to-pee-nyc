24254b5ce73a53b886a8329084eac13bf87fb831
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-05-06 21:44:37 -0400

    add phone number input

--- server/client/src/components/auth/SignUp.js
+++ server/client/src/components/auth/SignUp.js
@@ -1,21 +1,25 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { signup } from '../../actions';
+import 'react-phone-number-input/style.css'
 
+// "Without country select" component.
+import PhoneInput from "react-phone-number-input/react-hook-form-input"
 
 const userSchema = Yup.object().shape({
   email: Yup.string().email().required(),
   password: Yup.string().required(),
   name: Yup.string().required(),
-  phone: Yup.string().required(),
+  phone: Yup.string().required().length(12)
 });
 
 const SignUp = () => { 
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const { register, handleSubmit, control, formState: { errors } } = useForm({
     resolver: yupResolver(userSchema)
   });
 
@@ -33,50 +37,57 @@ const SignUp = () => {
   };
   
   return (
-    <form onSubmit={handleSubmit(handleSignUpSubmit)}>
-      <div className='form-group'>
-        <label>Email</label>
-        <input
-          className='form-control'
-          name='email'
-          {...register("email", {
-            required: "Required",
-          })}/>
-          {errors.email?.message}
-          {authError ? authError : ''}
-      </div>
-      <div className='form-group'>
-        <label>Name</label>
-        <input
-          className='form-control'
-          name='name'
-          {...register("name", {
-            required: "Required",
-          })}/>
-          {errors.name?.message}
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input 
-          className="form-control"
-          name='password'
-          {...register("password", {
-            required: "Required",
-          })}/>
-          {errors.password?.message}
-      </div>
-      <div className="form-group">
-        <label>Phone</label>
-        <input 
-          className="form-control"
-          name='phone'
-          {...register("phone", {
-            required: "Required",
-          })}/>
-          {errors.password?.message}
-      </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(handleSignUpSubmit)}>
+        <div className='form-group'>
+          <label>Email</label>
+          <input
+            className='form-control'
+            name='email'
+            {...register("email", {
+              required: "Required",
+            })}/>
+            {errors.email?.message}
+            {authError ? authError : ''}
+        </div>
+        <div className='form-group'>
+          <label>Name</label>
+          <input
+            className='form-control'
+            name='name'
+            {...register("name", {
+              required: "Required",
+            })}/>
+            {errors.name?.message}
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input 
+            className="form-control"
+            name='password'
+            {...register("password", {
+              required: "Required",
+            })}/>
+            {errors.password?.message}
+        </div>
+        <div className="form-group">
+          <label>Phone</label>
+          <PhoneInput
+            className="form-control"
+            name="phone"
+            country="US"
+            international
+            withCountryCallingCode
+            control={control}
+            rules={{ required: true}}
+           />
+          {errors.phone?.message}
+        </div>
+        <button className="btn btn-primary" type="submit">Submit</button>
+      </form>
+    </>
+    
+    
 )
 }
 
